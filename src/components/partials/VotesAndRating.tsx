dc1a65df11f9f7177abcda9630ba8ac0fdf825ec
Raymond Lam <raymond.m.lam@gmail.com>
2022-08-26 20:40:23 +0200

    added new partial components

---
+++ src/components/partials/VotesAndRating.tsx
@@ -0,0 +1,23 @@
+type VotesAndRatingProps = {
+	rating?: number;
+	votes?: number;
+};
+
+const VotesAndRating = ({ rating, votes }: VotesAndRatingProps) => {
+	return (
+		<div className="flex items-center">
+			{/* Rating */}
+			<p className="bg-blue-100 text-blue-800 text-sm font-semibold inline-flex items-center p-1.5 rounded">
+				{rating}
+			</p>
+
+			{/* Dot */}
+			<span className="mx-2 w-1 h-1 bg-indigo-600 rounded-full"></span>
+
+			{/* Votes */}
+			<p className="text-sm font-medium text-gray-500">{votes} votes</p>
+		</div>
+	);
+};
+
+export default VotesAndRating;
