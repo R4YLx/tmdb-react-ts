b203aee05672e1cc3dd662d042302665cd2c21db
Raymond Lam <raymond.m.lam@gmail.com>
2022-08-26 20:42:18 +0200

    created custom hook for now playing

--- src/hooks/useNowPlaying.ts
+++ src/hooks/useNowPlaying.ts
@@ -1,15 +1,10 @@
-import {
-	QueriesResults,
-	QueryKey,
-	useQuery,
-	UseQueryResult,
-} from "@tanstack/react-query";
+import { IMovie } from "./../interfaces/IMovie";
+import { useQuery } from "@tanstack/react-query";
 
 import TMDBAPI from "../services/TMDBAPI";
-import { IData } from "../interfaces/IData";
 
 const useNowPlaying = (page: string) => {
-	return useQuery<IData>(["now-playing", page], () =>
+	return useQuery<IMovie[]>(["now-playing", page], () =>
 		TMDBAPI.getNowPlayingMovies(page)
 	);
 };
